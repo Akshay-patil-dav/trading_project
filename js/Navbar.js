de3fadaf3e4be27c9e212b64793e3b230591ca60
Akshay Patil <akshaypatil22082002@gmail.com>
2024-09-16 14:51:43 +0530

    Add files via upload

---
+++ js/Navbar.js
@@ -0,0 +1,19 @@
+// Scroll effect: Change navbar color when user scrolls down
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {  // More than 50px scroll from top
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+}
+
+// Toggle menu for smaller screens
+const mobileMenu = document.getElementById("mobile-menu");
+const navLinks = document.querySelector(".nav-links");
+
+mobileMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
